62638b9b3f7f723128abbac7dee490e26c9532b1
ValerioCarbone <valeriocarbone10@gmail.com>
2024-01-15 09:42:03 +0100

    ADD - Many-tomany table project technology + models relations

--- app/Models/Admin/Project.php
+++ app/Models/Admin/Project.php
@@ -4,6 +4,7 @@ namespace App\Models\Admin;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use app\Models\Admin\Technology;
 
 class Project extends Model
 {
